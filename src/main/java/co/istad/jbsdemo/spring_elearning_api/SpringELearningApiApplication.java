48a0a32d49453cb242c2d8004512d648abb6d1c9
LAYHAK <layhak.garu@gmail.com>
2024-04-10 17:08:11 +0700

    first init

---
+++ src/main/java/co/istad/jbsdemo/spring_elearning_api/SpringELearningApiApplication.java
@@ -0,0 +1,13 @@
+package co.istad.jbsdemo.spring_elearning_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringELearningApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringELearningApiApplication.class, args);
+    }
+
+}
