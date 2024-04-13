f44f63f850fd9700cac303fc96c078e314b1b531
LAYHAK <layhak.garu@gmail.com>
2024-04-13 22:30:54 +0700

    Add Base Response and exception

---
+++ src/main/java/co/istad/jbsdemo/spring_elearning_api/base/BasedMessage.java
@@ -0,0 +1,12 @@
+package co.istad.jbsdemo.spring_elearning_api.base;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BasedMessage {
+    private String message;
+}
