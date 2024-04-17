8e9e7e30a477e308d04887b242e8c20d28526889
ONSOBEN <sobenkh35@gmail.com>
2024-04-17 23:46:04 +0700

    cities data

--- src/main/java/co/istad/jbsdemo/spring_elearning_api/feature/city/CityRepository.java
+++ src/main/java/co/istad/jbsdemo/spring_elearning_api/feature/city/CityRepository.java
@@ -1,8 +1,11 @@
 package co.istad.jbsdemo.spring_elearning_api.feature.city;
 
 import co.istad.jbsdemo.spring_elearning_api.domain.City;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +16,10 @@ public interface CityRepository extends JpaRepository<City, Long> {
     List<City> findAllByOrderByNameAsc();
 
     List<City> findByCountry_Iso(String iso);
+
+
+    @Query(value = "INSERT INTO cities (name, country_id) VALUES (:name, (SELECT id FROM countries WHERE iso = :iso))", nativeQuery = true)
+    @Modifying
+    @Transactional
+    void insertCity(@Param("name") String name, @Param("iso") String iso);
 }
